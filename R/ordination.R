2bdad89b3346a5e22ca3967027405bf61dcfe984
matutosi <matutosi@gmail.com>
2022-05-01 11:15:55 +0900

    first commit

---
+++ R/ordination.R
@@ -0,0 +1,44 @@
+ordination2 <- function(x, o_method, d_method = NULL, ...){
+  res <- list()
+  if(is.null(d_method)) d_method <- "bray"
+  if(o_method %in% c("pcoa", "nmds")) # compute dist when PCOA or nMDS
+    x <- vegan::vegdist(x, method = d_method)
+  switch(o_method,
+    "pca" = {
+      ord <- labdsv::pca(x, dim=10, ...)
+      res$scores  <- ord$scores
+      res$eig_val <- ord$sdev
+      res$d_method <- NULL
+    },
+    "ca" = {
+      ord <- vegan::cca(x)
+      res$scores  <- ord$CA$u
+      res$eig_val <- ord$CA$eig
+      res$d_method <- NULL
+    },
+    "dca" = {
+      ord <- vegan::decorana(x)
+      res$scores  <- ord$rproj
+      res$eig_val <- ord$evals
+      res$d_method <- NULL
+    },
+    "pcoa" = {
+      ord <- labdsv::pco(x)
+      res$scores  <- ord$points
+      res$eig_val <- ord$eig
+    },
+    "fspa" = {
+      ord <- dave::fspa(x, method = d_method, d.rev=0.5, n.groups=3)
+      res$scores  <- ord$newpoints
+      res$eig_val <- ord$eig
+    },
+    "nmds" = {
+      ord <- MASS::isoMDS(x)
+      res$scores  <- ord$points
+      res$eig_val <- NULL  # no eigen value
+    }
+  )
+  res$ordination_method <- o_method
+  res$distance_method <- d_method
+  return(res)
+}
