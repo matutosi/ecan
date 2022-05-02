9f0888d0799c54313b772035c8afa4bc6d6026eb
matutosi <matutosi@gmail.com>
2022-05-02 09:04:10 +0900

    etc

--- tests/testthat/test-ordination.R
+++ tests/testthat/test-ordination.R
@@ -1,6 +1,7 @@
 test_that("pca results match", {
   res_ord <- ordination(dune, o_method = "pca")
   res_pca <- labdsv::pca(dune, dim = 10)
-  expect_equal(res_ord$scores, res_pca$scores)
+  expect_equal(res_ord$st_scores, res_pca$scores)
+  expect_equal(res_ord$sp_scores, res_pca$loadings)
   expect_equal(res_ord$sdev,   res_pca$eig_val)
 })
