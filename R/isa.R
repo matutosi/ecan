5d25696edd53c8272207e437b44f8ffb31894b05
matutosi <matutosi@gmail.com>
2022-05-16 17:57:48 +0900

    add isa() completed! (maybe)

--- R/isa.R
+++ R/isa.R
@@ -6,53 +6,50 @@
 #' 
 #' @examples
 #' library(tidyverse)
+#' library(magrittr)
 #' library(vegan)
 #' data(dune)
 #' data(dune.env)
 #' df <- 
-#'   table2df(dune) %>%
+#'   dune %>%
+#'   table2df(st = "stand", sp = "species", ab = "cover") %>%
 #'   dplyr::left_join(tibble::rownames_to_column(dune.env, "stand"))
-#' df
-#'   
-#'   
-#' 
+#' isa(df, abundance = "cover", group = "Moisture", row_data = TRUE)
+#' isa(df, abundance = "cover", group = "Management")
+#' isa(df, abundance = "cover", group = "Use", row_data = TRUE)
+#' isa(df, abundance = "cover", group = "Manure")
 #' 
 #' @export
-
-df %>%
-  prep.isa(community="Management")
-isa <- function(df, stand = NULL, species = NULL, abundance = NULL, group = NULL){
+isa <- function(df, stand = NULL, species = NULL, abundance = NULL, group = NULL, row_data = FALSE){
+  # check inputs
   stopifnot(is.data.frame(df))
   if(is.null(stand))     stand     <- colnames(df)[1]
   if(is.null(species))   species   <- colnames(df)[2]
   if(is.null(abundance)) abundance <- colnames(df)[3]
   stopifnot(is.numeric(df[[abundance]]))
   if(is.null(group))     stop('Needs "group" input')
-  df %>%
-    dplyr::select(all_of(stand, species, abundance, group)) %>%
-    distinct() %>%
-  
-  
-}
-
-source("d:/matu/work/stat/r/matu2.r")	# MATSUMURA Toshikazu が作成した一般的に利用できる
-df$cover <- df$abundance
-
-## Indicator Spcies Analysis を簡単に実行する関数
-isa <- function(df, row.data=F){	# $stand, $species, $cover, $community
-	table <- tapply(df$cover, list(df$stand, df$species), sum)	# make compositional table
-	table[is.na(table)] <- 0
-	table <- data.frame(table)	#  to be coerced to data.frame
-	name.table <- colnames(table)	# original names of species
-	colnames(table) <- as.numeric(factor(colnames(table)))	# numerical names of species
-	names.table <- data.frame(species=name.table, numeric.sp=colnames(table))
-	com <- unique(subset(df, select=c(stand, community)))$community	# unique stand and community
-	com <- as.numeric(com)	# to be coerced to numeric
-	res <- labdsv::indval(table, com)	# Species Indicator Analysis
-	if(!row.data){
-		res <- data.frame(numeric.sp=names(res$maxcls), community=res$maxcls, ind.val=round(res$indcls,3), p.value=round(res$pval,4))
-		res <- merge(names.table, res)
-		res <- res[order(res$community, res$ind.val, res$p.value, decreasing=T),][,-1]
-	}
-	return(res)
+  # table
+  tbl <- df2table(df, st = stand, sp = species, ab = abundance)
+  # group
+  group_no <- paste0("numeric_", group)
+  gr <- 
+    tibble::tibble(`:=`({{stand}}, rownames(tbl))) %>%
+    dplyr::left_join(
+      dplyr::distinct(df, .data[[stand]], .data[[group]]), by = stand
+    ) %>%
+    dplyr::left_join(
+      tibble::tibble({{group}} := unique(df[[group]]), {{group_no}} := seq_along(unique(df[[group]]))), by = group
+    )
+  res <- labdsv::indval(tbl, gr[[group_no]])  # Species Indicator Analysis
+  if(!row_data){
+    res <- 
+      tibble::tibble(
+        {{species}} := names(res$maxcls), 
+        {{group}}   := res$maxcls, 
+        "ind.val"   := res$indcls, 
+        "p.value"   := res$pval
+      ) %>%
+      dplyr::arrange(.data[[group]], desc(res$ind.val), desc(res$p.value))
+  }
+  return(res)
 }
