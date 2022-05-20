270edf908235510c132c235a3e35e20bd9505ab8
matutosi <matutosi@gmail.com>
2022-05-21 06:34:51 +0900

    make new function pad2longest to add group in cluster

---
+++ R/utils_string.R
@@ -0,0 +1,15 @@
+#' Pad a string to the longest width of the strings.
+#' 
+#' @param string   Strings.
+#' @param side     Side on which padding character is added (left, right or both).
+#' @param pad      Single padding character (default is a space).
+#' 
+#' @example
+#' x <- c("a", "ab", "abc")
+#' pad2longest(x, side = "right", pad = " "){
+#' 
+#' @export
+pad2longest <- function(string, side = "right", pad = " "){
+  width <- max(stringr::str_length(string))
+  stringr::str_pad(string, width = width, side = side, pad = pad)
+}
