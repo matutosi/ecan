ed333e74c1a450e319c71067bf3ef0a3887f0d08
matutosi <matutosi@gmail.com>
2022-05-21 06:33:09 +0900

    add cls_add_group()

--- R/clustering.R
+++ R/clustering.R
@@ -19,11 +19,33 @@
 #'          $distance_method:   d_method
 #' @examples
 #' library(vegan)
+#' library(ggdendro)
 #' data(dune)
-#' clustering(dune, c_method = "average", d_method = "euclidean")
+#' data(dune.env)
+#' 
+#' df <- 
+#'   table2df(dune) %>%
+#'   dplyr::left_join(tibble::rownames_to_column(dune.env, "stand"))
+#' sp_dammy <- 
+#'  tibble::tibble("species" = colnames(dune), 
+#'                 "dammy_1" = stringr::str_sub(colnames(dune), 1, 1),
+#'                 "dammy_6" = stringr::str_sub(colnames(dune), 6, 6))
+#' df <- 
+#'   df %>%
+#'   dplyr::left_join(sp_dammy)
+#' 
+#' cls <- cluster(dune, c_method = "average", d_method = "euclidean")
+#' ggdendrogram(cls)
+#' 
+#' cls$labels <- 
+#'   cls_add_group(cls, df, single = "stand", group = "Use")
+#' ggdendrogram(cls)
+#' 
+#' 
+#' 
 #' 
 #' @export
-clustering <- function(x, c_method, d_method){
+cluster <- function(x, c_method, d_method){
   cls <- list()
     # distance
   if(d_method == "correlation"){
@@ -52,4 +74,23 @@ clustering <- function(x, c_method, d_method){
 #' @export
 t_if_true <- function(x, cond){
   if(cond) t(x) else x
+}
+
+
+#' Transfer when true
+#' 
+#' @param cls      A result of cluster analysis.
+#' @inheritParams  ordination
+#' @rdname         cluster
+#' @export
+cls_add_group <- function(cls, df, single, group){
+  single_group <- 
+    tibble::tibble(`:=`({{single}}, cls$labels)) %>%
+    dplyr::left_join(dplyr::distinct(df, .data[[single]], .data[[group]]))
+  lab_1 <- 
+    single_group[[group]] %>%
+    pad2longest(side = "right", pad = "_")
+  lab_2 <- pad2longest(cls$labels, side = "left", pad = "_")
+  new_lab <- stringr::str_c(lab_1 , "-", lab_2)
+  return(new_lab)
 }
