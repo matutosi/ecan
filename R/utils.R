60cc3a20fa56acf3da29ce6203f3282f4b41f86c
matutosi <matutosi@gmail.com>
2022-05-23 09:36:15 +0900

    rename file

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,7 @@
 #' @param side     Side on which padding character is added (left, right or both).
 #' @param pad      Single padding character (default is a space).
 #' 
+#' @return         Strings.
 #' @examples
 #' x <- c("a", "ab", "abc")
 #' pad2longest(x, side = "right", pad = " ")
@@ -12,4 +13,18 @@
 pad2longest <- function(string, side = "right", pad = " "){
   width <- max(stringr::str_length(string))
   stringr::str_pad(string, width = width, side = side, pad = pad)
+}
+
+#' Pad a string to the longest width of the strings.
+#' 
+#' @param x        A numeric vector.
+#' @return         A factor vector.
+#' @examples
+#' x <- runif(100)
+#' cut_conti(x)
+#' 
+#' @export
+cut_conti <- function(x){
+  brks <- hist(x, plot = FALSE)$breaks
+  cut(x, brks)
 }
