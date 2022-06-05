5e293a753a375e06ed7cfcd75a133beac518d19f
matutosi <matutosi@gmail.com>
2022-06-05 20:10:41 +0900

    add test

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,6 @@ library(testthat)
 library(tidyverse)
 library(ecan)
 library(vegan)
-
 data(dune)
 
 test_check("ecan")
---
+++ tests/testthat/layer_construction.R
@@ -0,0 +1,7 @@
+test_that("pca results match", {
+  height    <- c(2, 4, 8, 20)
+  mid_point <- c(1, 3, 6, 14)
+  bin_width <- c(2, 2, 4, 12)
+  expect_equal(mid_point(height), mid_point)
+  expect_equal(bin_width(height), bin_width)
+})
