f832fc0e1eec512bf35f09ee85b91529477a231e
matutosi <matutosi@gmail.com>
2022-06-07 06:55:56 +0900

    add function to generate example

---
+++ R/gen_example_layer.R
@@ -0,0 +1,84 @@
+#' Generate example with layer
+#' 
+#' 
+#' 
+#' 
+#' @param n            A numeric to generate no of occurances.
+#' @param use_layer    A logical. If FALSE, height_max and ly_list will be omitted.
+#' @param height_max   A numeric. The highest layer of samples.
+#' @param ly_list,st_list,sp_list,st_group,sp_group
+#'                     A string vector. 
+#'                     st_group and sp_group are optional.
+#'                     Lenght of st_list and st_group should be the same.
+#'                     Lenght of sp_list and sp_group should be the same.
+#' @param cover_list   A numeric vector.
+#' 
+#' @return  A dataframe with columns: stand, layer, species, cover, st_group and sp_group
+#' 
+#' @examples
+#' n <- 300
+#' height_max <- 20
+#' ly_list    <- c("B1", "B2", "S1", "S2", "K")
+#' st_list    <- LETTERS[1:9]
+#' sp_list    <- letters[1:9]
+#' st_group   <- rep(LETTERS[24:26], 3)
+#' sp_group   <- rep(letters[24:26], 3)
+#' cover_list <- 2^(0:6)
+#' gen_example_layer(n = n, 
+#'                   height_max = height_max, ly_list = ly_list, 
+#'                   st_list  = st_list,  sp_list  = sp_list,
+#'                   st_group = st_group, sp_group = sp_group,
+#'                   cover_list = cover_list)
+#' 
+#' @export
+gen_example_layer <- function(n = 300, 
+                              use_layer  = TRUE,
+                              height_max = 20,
+                              ly_list    = c("B1", "B2", "S1", "S2", "K"),
+                              st_list    = LETTERS[1:9],
+                              sp_list    = letters[1:9],
+                              st_group   = NULL,
+                              sp_group   = NULL,
+                              cover_list = 2^(0:6)){
+  cover <- sample(cover_list, size = n, replace = TRUE, 
+                  prob = log(2^(length(cover_list):1)))
+  if(use_layer){
+    layer <- 
+      tibble::tibble(
+      stand  = rep(st_list, times = length(ly_list)),
+      layer  = rep(ly_list, each  = length(st_list)),
+      height = 
+        sample(1:(height_max * 10) / 10, length(ly_list) * length(st_list)) %>%
+        sort(decreasing = TRUE))
+  }
+  stand <- tibble::tibble(stand = st_list, st_group)
+  if(use_layer) stand <- dplyr::left_join(stand, layer)
+  species <- tibble::tibble(species = sp_list, sp_group)
+  comp <- 
+    tibble::tibble(
+    stand   = sample(st_list, n, replace = TRUE),
+    species = sample(sp_list, n, replace = TRUE),
+    cover   = sample(2^(0:6), size = n, replace = TRUE, prob = log(2^(7:1))))
+  if(use_layer){
+    comp <- 
+      comp %>%
+      bind_cols(tibble::tibble(layer = sample(ly_list, n, replace = TRUE)))
+  }
+  comp <- 
+    {if(use_layer){
+      dplyr::group_by(comp, stand, layer, species)
+    } else {
+      dplyr::group_by(comp, stand, species)
+    }} %>%
+    dplyr::summarise(cover = sum(cover), .groups = "drop") %>%
+    dplyr::ungroup()
+  df <- 
+    {if(use_layer){
+      dplyr::arrange(comp, stand, layer, species)
+    } else {
+      dplyr::arrange(comp, stand, species)
+    }} %>%
+    dplyr::left_join(stand) %>%
+    dplyr::left_join(species)
+  return(df)
+}
